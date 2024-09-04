528f6685a85a6fff82eeae8095967b28879c35ce
izzy-2023 <izzyzinxhirija@gmail.com>
2024-09-03 21:32:23 -0400

    updated todo list file

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,34 +1,15 @@
 // app/components/TodoList.js
-import { useState } from 'react';
-
-const TodoList = () => {
-    const [items, setItems] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-
-    const handleAddItem = () => {
-        if (inputValue.trim() !== '') {
-            setItems([...items, inputValue]);
-            setInputValue('');
-        }
-    };
-
-    return (
-        <div>
-            <h2>To-Do List</h2>
-            <input 
-                type="text" 
-                value={inputValue} 
-                onChange={(e) => setInputValue(e.target.value)} 
-                placeholder="Add a new item"
-            />
-            <button onClick={handleAddItem}>Add</button>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
+const TodoList = ({ items }) => {
+  return (
+      <div>
+          <h2>To-Do List</h2>
+          <ul>
+              {items.map((item, index) => (
+                  <li key={index}>{item}</li>
+              ))}
+          </ul>
+      </div>
+  );
 };
 
 export default TodoList;
